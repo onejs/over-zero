3afc35226bdebe0f4050c749b1dafc72e72832a5
natew <natewienert@gmail.com>
2026-02-01 10:20:09 -1000

    clean types

---
+++ types/constants.native.d.ts
@@ -0,0 +1,3 @@
+export declare const isServer = false;
+export declare const isBrowser = false;
+//# sourceMappingURL=constants.native.d.ts.map
--- types/where.d.ts
+++ types/where.d.ts
@@ -1,5 +1,5 @@
-import type { TableName, Where } from './types';
 import type { Condition } from '@rocicorp/zero';
+import type { TableName, Where } from './types';
 export declare function where<Table extends TableName, Builder extends Where<Table>>(tableName: Table, builder: Builder, isServerOnly?: boolean): Where<Table, Condition>;
 export declare function where<Table extends TableName, Builder extends Where = Where<Table>>(builder: Builder): Where<Table, Condition>;
 export declare function getWhereTableName(where: Where): string | undefined;
